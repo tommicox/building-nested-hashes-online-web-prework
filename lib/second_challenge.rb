3f6e1aae92e2053606ace665cffcc610b7941886
Tommi Cox <tommicox@gmail.com>
2019-03-07 20:34:51 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,19 +2,23 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }, 
+    }, 
     :capulet => {
       #your key/value pairs here!
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
+      
       }
-    
+      epic_tragedy = {
+        :montague => {
+          :patriarch => {},
+          :matriarch => {},
+          :hero => {},
+          :hero_friends => []
+      }
+      :capulet => {
+         :patriarch => {},
+         :matriarch => {},
+         :heroine => {},
+         :heroine_friends => []
   }
 
   
