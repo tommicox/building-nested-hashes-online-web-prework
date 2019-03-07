dbf5bb70da40142d4d1ccca66a4b6509f4f965dc
Tommi Cox <tommicox@gmail.com>
2019-03-07 20:45:22 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -33,7 +33,17 @@ def third_challenge
    }
   }
  epic_tragedy = {
-   :montague
+   :montague => {
+     :patriarch => {name: "Lord Montague", age: "53"}
+     :matriarch => {name: "Lady Montague", age: "54"}
+     :hero => {name: "Romeo", age: "15", status: "alive"}
+     :hero_friends => []
+   }
+   :capulet => {
+     :patriarch => {name: "Lord Capulet", age: "50"}
+     :matriarch => {name: "Lady Capulet", age: "51"}
+     :heroine => {name: "Juliet", 
+   }
  }
   
 end
